acc99d245c0f6052e9de3aac9dd164eee8e47113
Tiago <tiagolachman@gmail.com>
2025-03-22 15:34:10 -0300

    Initial commit

---
+++ src/main/java/com/pos/webserviceprimos/Endpoint.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pos.webserviceprimos;
+
+import jakarta.websocket.server.PathParam;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Tiago André Lachman
+ */
+@RestController
+@RequestMapping("/AppWebConf2/resources")
+public class Endpoint {
+
+    @GetMapping("/servicoRest/{v}")
+    public String servicoRest(@PathParam("v") int v) {
+        return primos(v).stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(", "));
+    }
+
+    private List<Integer> primos(int n) {
+        List<Integer> primos = new ArrayList<>();
+        if (n > 1) {
+            primos.add(1);
+        }
+        for (int i = 2; i < n; i++) {
+            if (ehPrimo(i)) {
+                primos.add(i);
+            }
+        }
+        return primos;
+    }
+
+    private boolean ehPrimo(int num) {
+        if (num < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ src/main/java/com/pos/webserviceprimos/WebServicePrimos.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.pos.webserviceprimos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Tiago André Lachman
+ */
+@SpringBootApplication
+public class WebServicePrimos {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServicePrimos.class, args);
+    }
+}
